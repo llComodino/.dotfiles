1e88450bac9f39f8d83c0ed78a098a585a3d77d1
llComodino <comodinocode@proton.me>
2024-05-25 23:38:46 +0200

    Sync Sat May 25 23:38:46 CEST 2024

--- nvim/.config/nvim/after/plugin/luasnip.lua
+++ nvim/.config/nvim/after/plugin/luasnip.lua
@@ -1,1 +1,3 @@
-require("luasnip.loaders.from_snipmate").lazy_load({paths = "~/.config/nvim/after/plugin/snippets"})
+require("luasnip.loaders.from_snipmate").lazy_load({
+  paths = "~/.config/nvim/after/plugin/snippets"
+})
